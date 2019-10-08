68582570e544dcfff0ca4f77b762bdd73f5a033e
SergeyProjDevOfficial <s1712@tmm-sapr.org>
2019-10-08 09:21:11 +0300

    simple parse test

--- MetanitCSharpParserUWP/MetanitCSharpParserUWP/MainPage.xaml.cs
+++ MetanitCSharpParserUWP/MetanitCSharpParserUWP/MainPage.xaml.cs
@@ -1,17 +1,11 @@
-﻿using System;
+﻿using AngleSharp;
+using AngleSharp.Dom;
+using System;
+using System.Collections;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -26,5 +20,34 @@ namespace MetanitCSharpParserUWP
         {
             this.InitializeComponent();
         }
+
+        private async void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Setup the configuration to support document loading
+            var config = Configuration.Default.WithDefaultLoader();
+
+            // Load the names of all The Big Bang Theory episodes from Wikipedia
+            var address = "https://metanit.com/sharp/mvc5/1.1.php";
+            // Asynchronously get the document in a new context using the configuration
+            var document = await BrowsingContext.New(config).OpenAsync(address);
+
+
+            // Perform the query to get all cells with the content
+            //var menuItems = document.QuerySelectorAll("span").Where(m => m.ClassName.Contains("folder"));
+            IEnumerable<IElement> menuItems = from item in document.QuerySelectorAll("span")
+                            where item.ClassName.Contains("folder")
+                            select item;
+
+            // We are only interested in the text - select it with LINQ
+            var titles = menuItems.Select(m => m.TextContent).ToList();
+
+            foreach (var item in titles)
+                folders.Items.Add(item);
+        }
+
+
+        // open SplitView panel
+        private void Button_Click(object sender, RoutedEventArgs e) => 
+            splitView.IsPaneOpen = !splitView.IsPaneOpen;
     }
 }
